d964f2e5ad0fdfbdc279b8fe4f49136eb5c84b5f
sheila-NAA <59737033+sheila-NAA@users.noreply.github.com>
2021-04-06 16:01:42 +0000

    Update test_eq_clean_data.R

--- testthat/test_eq_clean_data.R
+++ testthat/test_eq_clean_data.R
@@ -1,41 +1,39 @@
-context("test-eq_clean_data")
+context('clean data')
 
-tidy_noaa <- eq_clean_data(noaa)
+# load quakes from package
+df <- quakes
 
+test_that('basic function works correctly', {
 
-test_that("output is a data.frame", {
-  expect_is(tidy_noaa, "data.frame")
+  df_clean <- eq_clean_data(df)
+
+  expect_equal(ncol(df) + 1, ncol(df_clean))
+  expect_equal(nrow(df), nrow(df_clean))
+  expect_is(df_clean$DATE, 'Date')
+  expect_is(df_clean$LATITUDE, 'numeric')
+  expect_is(df_clean$LONGITUDE, 'numeric')
+  expect_is(df_clean$DEATHS, 'numeric')
+  expect_is(df_clean$EQ_PRIMARY, 'numeric')
+
 })
 
+test_that('throws error if do not have right columns', {
 
-test_that("lowercase columns", {
-  noaa_names <- names(tidy_noaa)
+  expect_error(eq_clean_data(df %>% dplyr::select(-YEAR)),
+               'Missing required variable: YEAR')
+  expect_error(eq_clean_data(df %>% dplyr::select(-MONTH)),
+               'Missing required variable: MONTH')
+  expect_error(eq_clean_data(df %>% dplyr::select(-DAY)),
+               'Missing required variable: DAY')
+  expect_error(eq_clean_data(df %>% dplyr::select(-LATITUDE)),
+               'Missing required variable: LATITUDE')
+  expect_error(eq_clean_data(df %>% dplyr::select(-LONGITUDE)),
+               'Missing required variable: LONGITUDE')
+  expect_error(eq_clean_data(df %>% dplyr::select(-DEATHS)),
+               'Missing required variable: DEATHS')
+  expect_error(eq_clean_data(df %>% dplyr::select(-EQ_PRIMARY)),
+               'Missing required variable: EQ_PRIMARY')
 
-  expect_equal(noaa_names, tolower(noaa_names))
-})
-
-
-test_that("correct column date", {
-  data_column <- tidy_noaa[['date']]
-
-  expect_is(data_column, "Date")
-})
-
-
-test_that("latitude and longitude columns converted to numeric class", {
-  latitude_column  <- tidy_noaa[['latitude']]
-  longitude_column <- tidy_noaa[['longitude']]
-
-  expect_is(latitude_column, "numeric")
-  expect_is(longitude_column, "numeric")
-})
-
-
-test_that("location_names are cleaned", {
-  location_column <- tidy_noaa[['location_name']]
-
-  expect_equal(
-    location_column,
-    eq_location_clean(noaa$LOCATION_NAME)
-  )
+  # remove a column we don't care much about
+  expect_is(eq_clean_data(df %>% dplyr::select(-MINUTE)), 'tbl_df')
 })
