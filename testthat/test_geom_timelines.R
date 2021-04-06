50fccb3da5570c8d072e06201f92b91fe85cbe0e
sheila-NAA <59737033+sheila-NAA@users.noreply.github.com>
2021-04-06 16:03:21 +0000

    Update test_geom_timelines.R

--- testthat/test_geom_timelines.R
+++ testthat/test_geom_timelines.R
@@ -1,17 +1,25 @@
-context("test-geom_timeline")
+context('eq_timeline shortcut')
 
-gg_output <- noaa %>%
-  eq_clean_data() %>%
-  dplyr::filter(country == "GREECE", lubridate::year(date) >= 2000) %>%
-  ggplot2::ggplot(ggplot2::aes(
-    x = date,
-    y = country,
-    size   = eq_primary,
-    colour = log(total_deaths)
-  )) +
-  geom_timeline()
+test_that('geom_timeline works, allows 2 countries', {
+  quakes <- eq_load_clean_data()
 
+  p1 <- eq_timeline(quakes)
+  expect_s3_class(p1, 'ggplot')
+  out1 <- layer_data(p1)
+  expect_length(p1$layers, 1)
+  expect_length(unique(out1$group), 1)
+  #expect_true()
 
-test_that("correct object in output", {
-  expect_is(gg_output, "gg")
+  # can handle lowercase country names and 5 labels:
+  p2 <- eq_timeline(quakes, countries = c('USA', 'canada'), label_n = 5)
+  expect_length(p2$layers, 2)
+  out2 <- layer_data(p2, 2)
+  expect_true('label' %in% names(out2))
+
+  # date range change works
+  p3 <- eq_timeline(quakes, countries = c('USA', 'canada'),
+                    date_min = '1950-01-01', label_n = 5)
+  out3 <- layer_data(p3, 2)
+  expect_gt(nrow(out3), nrow(out2))
+
 })
